806fde9b8311b0baee0bd3966859fe6137306120
Jensen <32688924+Jensenhit@users.noreply.github.com>
2021-02-13 18:52:39 +0800

    palindrome string

--- 6_hash-table-and-string_palindrome-string.cpp
+++ 6_hash-table-and-string_palindrome-string.cpp
@@ -3,7 +3,34 @@ hash table
 find longest palindrome string
 */
 
-#include
+#include <iostream>
+using namespace std;
 
+class Solution{
+public:
+    int longestPalindrome(string s){
+        int char_map[128] = {0};
+        int max_length = 0;
+        int flag = 0;
+        for (int i = 0; i < s.length(); i++){
+            char_map[s[i]]++;
+        }
+        for (int i = 0; i < 128; i++){
+            if (char_map[i]%2 == 0){
+                max_length += char_map[i];
+            }
+            else{
+                max_length += char_map[i]-1;
+                flag = 1;
+            }
+        }
+        return max_length+flag;
+    }
+};
 
-
+int main(){
+    string s = "abccccddaa";
+    Solution solve;
+    cout<<solve.longestPalindrome(s)<<endl;
+    return 0;
+}
