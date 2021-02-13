e74ac302ca60f2109b27702066bfa55bc0a7be00
Jensen <32688924+Jensenhit@users.noreply.github.com>
2021-02-13 19:09:50 +0800

    word pattern

--- 6_hash-table-and-string_words.cpp
+++ 6_hash-table-and-string_words.cpp
@@ -1,9 +1,48 @@
 /*
 hash table
-words
+words pattern
 */
 
-#include
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
 
+class Solution{
+public:
+    bool wordPattern(string pattern, string str){
+        map<string, char> word_map;
+        char used[128] = {0};
+        string word;
+        int pos = 0;
+        str.push_back(' ');
+        for (int i = 0; i < str.length(); i++){
+            if (str[i] == ' '){
+                if (pos == pattern.length()) return false;
+                if (word_map.find(word) == word_map.end()){
+                    if (used[pattern[pos]]) return false;
+                    word_map[word] = pattern[pos];
+                    used[pattern[pos]] = 1;
+                }
+                else{
+                    if (word_map[word] != pattern[pos]) return false;
+                }
+                word = "";
+                pos++;
+            }
+            else{
+                word += str[i];
+            }
+        }
+        if (pos != pattern.length()) return false;
+        return true;
+    }
+};
 
-
+int main(){
+    string pattern = "abba";
+    string str = "dog cat cat dog";
+    Solution solve;
+    cout<<solve.wordPattern(pattern, str)<<endl;
+    return 0;
+}
