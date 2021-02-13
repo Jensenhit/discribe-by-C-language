c0f102fded9a1ddd08a0c7af86076f396af63538
Jensen <32688924+Jensenhit@users.noreply.github.com>
2021-02-13 11:13:59 +0800

    maximum subset sum

--- 7_dynamic_miximum-subset-sum.cpp
+++ 7_dynamic_miximum-subset-sum.cpp
@@ -3,7 +3,38 @@ dynamic programming
 maximum subset sum
 */
 
-#include
+#include <iostream>
+#include <vector>
+using namespace std;
 
+class Solution{
+public:
+    int maxSubArray(vector<int>& nums){
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        int max_res = dp[0];
+        for (int i = 1; i < nums.size(); i++){
+            dp[i] = max(dp[i-1]+nums[i], nums[i]);
+            if (max_res < dp[i]){
+                max_res = dp[i];
+            }
+        }
+        return max_res;
+    }
+};
 
-
+int main(){
+    Solution solve;
+    vector<int> nums;
+    nums.push_back(-2);
+    nums.push_back(1);
+    nums.push_back(-3);
+    nums.push_back(4);
+    nums.push_back(-1);
+    nums.push_back(2);
+    nums.push_back(1);
+    nums.push_back(-5);
+    nums.push_back(4);
+    cout<<solve.maxSubArray(nums)<<endl;
+    return 0;
+}
