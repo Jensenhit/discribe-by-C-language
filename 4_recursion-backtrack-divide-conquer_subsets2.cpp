80ad5683c3b46f59a1f814329350187f29b22eef
Jensen <32688924+Jensenhit@users.noreply.github.com>
2021-02-13 13:29:31 +0800

    subsets by backtracking

--- 4_recursion-backtrack-divide-conquer_subsets2.cpp
+++ 4_recursion-backtrack-divide-conquer_subsets2.cpp
@@ -3,7 +3,54 @@ recursion, backtracking, divide and conquer
 subsets by backtracking
 */
 
-#include
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
 
+class Solution{
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums){
+        vector<vector<int>> result;
+        vector<int> item;
+        set<vector<int>> res_set;
+        sort(nums.begin(), nums.end());
+        result.push_back(item);
+        generate(0, nums, result, item, res_set);
+        return result;
+    }
+private:
+    void generate(int i, vector<int>& nums, vector<vector<int>> &result, vector<int> &item, set<vector<int>> &res_set){
+        if (i >= nums.size()){
+            return;
+        }
+        item.push_back(nums[i]);
+        if (res_set.find(item) == res_set.end()){
+            result.push_back(item);
+            res_set.insert(item);
+        }
+        generate(i+1, nums, result, item, res_set);
+        item.pop_back();
+        generate(i+1, nums, result, item, res_set);
+    }
+};
 
-
+int main(){
+    vector<int> nums;
+    nums.push_back(2);
+    nums.push_back(1);
+    nums.push_back(2);
+    nums.push_back(2);
+    vector<vector<int>> result;
+    Solution solve;
+    result = solve.subsetsWithDup(nums);
+    for (int i = 0; i < result.size(); i++){
+        if (result[i].size() == 0) cout<<"[]";
+        for (int j = 0; j < result[i].size(); j++){
+            cout<<"["<<result[i][j]<<"]";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
