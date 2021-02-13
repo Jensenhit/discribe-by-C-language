c313c8d2402474d03fa5767945bf4a5aa82ef101
Jensen <32688924+Jensenhit@users.noreply.github.com>
2021-02-13 10:54:00 +0800

    climb stairs by dynamic programming

--- 7_dynamic_climb-stairs.cpp
+++ 7_dynamic_climb-stairs.cpp
@@ -3,7 +3,27 @@ dynamic programming
 climb stairs
 */
 
-#include
+#include <iostream>
+#include <vector>
+using namespace std;
 
+class Solution{
+public:
+    int climbStairs(int n){
+        vector<int> dp(n+3, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
 
-
+int main(){
+    int m = 3;
+    Solution solve;
+    int res = solve.climbStairs(m);
+    cout<<res<<endl;
+    return 0;
+}
